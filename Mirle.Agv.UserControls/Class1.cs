9d221f983282e879e9234112c3ef675ea033c2a9
Lien6410 <Lien6410@gmail.com>
2020-08-12 16:11:15 +0800

    加入專案檔案。

---
+++ Mirle.Agv.UserControls/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mirle.Agv.UserControls
+{
+    public class Class1
+    {
+    }
+}
